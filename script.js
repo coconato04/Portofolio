ea9cf6a7a7160ce11f193953a87189bfead42b28
ardiansyah jaya winata - 535210014 <95003098+coconato04@users.noreply.github.com>
2025-02-04 17:53:03 +0700

    up 2 ( penyelesaian profile)

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+// Fungsi untuk Memuat Halaman ke dalam #content
 function loadPage(page) {
     fetch(page)
         .then(response => response.text())
         .then(data => {
             document.getElementById("content").innerHTML = data;
         })
-        .catch(error => console.log("Error loading the page: ", error));
+        .catch(error => console.error("Error loading the page: ", error));
 }
+
+// Pastikan konten awal langsung dimuat saat halaman dibuka
+document.addEventListener("DOMContentLoaded", function() {
+    loadPage("profile.html"); // Default memuat halaman Profile
+});
